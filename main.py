868b129361ad3e03b3653b6fe6047d99207e2bbd
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-10-04 22:05:15 -0700

    Adding Passwords.text file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-A = 'small'
+def check_password(password: str):
+    with open('passwords.text', 'r') as file:
+        common_passwords: list[str] = file.read().splitlines()
+        print(common_passwords)
+
+
+def main():
+    check_password('abc123')
+
+if __name__ == '__main__':
+    main()
