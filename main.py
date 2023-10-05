5d130e8ecddd0b408f4a14b380b7d9928d859569
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-10-04 23:12:18 -0700

    Added input verification

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import pyinputplus
+
 def check_password(password: str):
     with open('passwords.text', 'r') as file:
         common_passwords: list[str] = file.read().splitlines()
-        print(common_passwords)
+
+    for i, common_passwords in enumerate(common_passwords, start=1):
+        if password == common_passwords:
+            print(f'{password}: Match Found (#{i})')
+        return
+
+    print(f'{password}: Match Not Found')
 
 
 def main():
-    check_password('abc123')
+    user_password: str = pyinputplus.inputStr('Enter Password: ')
+    check_password(user_password)
+
 
 if __name__ == '__main__':
     main()
